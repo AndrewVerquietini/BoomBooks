2e59c22795e9c238eae9bce218d3497afa150f16
Andrew Verquietini <andrew212001@gmail.com>
2023-10-26 23:09:23 -0300

    Entrega03(gui): Login funcionando
    Login funcionando, validando tanto usuário admin quanto comum e tela de cadastro funcionando.

--- BoomBook/src/main/java/BD/ConexaoBD.java
+++ BoomBook/src/main/java/BD/ConexaoBD.java
@@ -1,28 +1,101 @@
 package BD;
 
+import TelaLogin.Usuario;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class ConexaoBD {
-    public static void main (String [] args) throws SQLException {
-         Connection conexao = DriverManager.getConnection("jdbc:mysql://localhost/BoomBooksBD", "root", "123456789");
+    private static final String URL = "jdbc:mysql://localhost/BoomBooksBD";
+    private static final String USUARIO = "root";
+    private static final String SENHA = "123456789";
 
-        try{
-                Class.forName("com.mysql.jdbc.Driver");
-                ResultSet rsCliente = conexao.createStatement().executeQuery("SELECT * FROM usuario_comum where nome = 'Alexandre'");
-                while (rsCliente.next()){
-                            System.out.println("Nome " + rsCliente.getString("nome"));
-                }
-        }catch (ClassNotFoundException ex){
-                System.out.println("Driver do banco de dados não localizado.");
-        }catch (SQLException ex){
-                System.out.println("Falha ao acessar o banco:" + ex.getMessage());
-        }finally {
-                if (conexao != null){
-                    conexao.close();
-                }
+    public static Connection obterConexao() throws SQLException {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver"); // Carrega o driver do MySQL
+
+            Connection conexao = DriverManager.getConnection(URL, USUARIO, SENHA);
+            return conexao;
+        } catch (ClassNotFoundException e) {
+            throw new SQLException("Driver MySQL não encontrado", e);
         }
+    }
+
+    public static void fecharConexao(Connection conexao) {
+        if (conexao != null) {
+            try {
+                conexao.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    
+    public boolean validarLoginComum(String email, String senha) {
+        try {
+            Connection conexao = obterConexao();
+            
+            // Execute a consulta SQL para verificar as credenciais de usuário comum
+            String query = "SELECT * FROM usuario WHERE email = ? AND senha = ?";
+            PreparedStatement stmt = conexao.prepareStatement(query);
+            stmt.setString(1, email);
+            stmt.setString(2, senha);
+            ResultSet rs = stmt.executeQuery();
+            
+            return rs.next(); // Se houver uma linha correspondente, o login é válido.
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false; // Em caso de erro, retorne falso.
+        }
+    }
+
+    public boolean validarLoginAdmin(String email, String senha) {
+        try {
+            Connection conexao = obterConexao();
+            
+            // Execute a consulta SQL para verificar as credenciais de administrador
+            String query = "SELECT * FROM usuario WHERE email = ? AND senha = ? AND tipo = 'admin'";
+            PreparedStatement stmt = conexao.prepareStatement(query);
+            stmt.setString(1, email);
+            stmt.setString(2, senha);
+            ResultSet rs = stmt.executeQuery();
+            
+            return rs.next(); // Se houver uma linha correspondente, o login é válido.
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false; // Em caso de erro, retorne falso.
+        }
+    }
+    
+    public static void inserirUsuario(Usuario usuario) throws SQLException {
+        Connection conexao = null;
+        PreparedStatement stmt = null;
+
+        try {
+            conexao = obterConexao();
+            String sql = "INSERT INTO usuario (nome, email, senha, genero, idade) VALUES (?, ?, ?, ?, ?)";
+            stmt = conexao.prepareStatement(sql);
+
+            stmt.setString(1, usuario.getNome());
+            stmt.setString(2, usuario.getEmail());
+            stmt.setString(3, usuario.getSenha());
+            stmt.setString(4, usuario.getGenero());
+            stmt.setInt(5, usuario.getIdade());
+
+            stmt.executeUpdate();
+        } finally {
+            if (stmt != null) {
+                stmt.close();
+            }
+            if (conexao != null) {
+                fecharConexao(conexao);
+            }
+        }
+    }
+    
+    public void desconectar() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
 }
---
+++ BoomBook/src/main/java/TelaLogin/Usuario.java
@@ -0,0 +1,58 @@
+package TelaLogin;
+
+public class Usuario {
+    private String nome;
+    private String email;
+    private String senha;
+    private String genero;
+    private int idade;
+
+    public Usuario(String nome, String email, String senha, String genero, int idade) {
+        this.nome = nome;
+        this.email = email;
+        this.senha = senha;
+        this.genero = genero;
+        this.idade = idade;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+ 
+}
